9f15247dab017653ebb7a87f1708d3d153665b36
Mariugo Megiani <mariugomegiani@gmail.com>
2021-11-06 19:07:26 -0300

    Auth provider impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:babble/providers/authentication_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
@@ -22,21 +24,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Babble',
-      theme: ThemeData(
-        backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
-        scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationProvider>(
+          create: (BuildContext context) => AuthenticationProvider(),
         ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Babble',
+        theme: ThemeData(
+          backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+          scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),
+          ),
+        ),
+        navigatorKey: NavigationService.navigatorKey,
+        initialRoute: '/login',
+        routes: {
+          '/login': (BuildContext _context) => const LoginScreen(),
+        },
       ),
-      navigatorKey: NavigationService.navigatorKey,
-      initialRoute: '/login',
-      routes: {
-        '/login': (BuildContext _context) => const LoginScreen(),
-      },
     );
   }
 }
