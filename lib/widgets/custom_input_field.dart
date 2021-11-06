2ef332fc5962fcd7d1841a09e1f392d488a32356
Mariugo Megiani <mariugomegiani@gmail.com>
2021-11-06 17:09:57 -0300

    Custom input done

--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -25,6 +25,62 @@ class CustomInputField extends StatelessWidget {
       obscureText: obscureText,
       validator: (_value) =>
           RegExp(regEx).hasMatch(_value!) ? null : 'Enter a valid value',
+      decoration: InputDecoration(
+        fillColor: const Color.fromRGBO(
+          30,
+          29,
+          37,
+          1.0,
+        ),
+        filled: true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
+        ),
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: Colors.white54,
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    Key? key,
+    required this.onEditingComplete,
+    required this.hintText,
+    required this.obscureText,
+    required this.controller,
+    required this.icon,
+  }) : super(key: key);
+
+  final Function(String) onEditingComplete;
+  final String hintText;
+  final bool obscureText;
+  final TextEditingController controller;
+  final IconData? icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      onEditingComplete: () => onEditingComplete(controller.value.text),
+      cursorColor: Colors.white,
+      style: const TextStyle(color: Colors.white),
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        fillColor: const Color.fromRGBO(30, 29, 37, 1.0),
+        filled: true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide.none,
+        ),
+        hintText: hintText,
+        hintStyle: const TextStyle(color: Colors.white54),
+        prefixIcon: Icon(icon, color: Colors.white54),
+      ),
     );
   }
 }
